ec02d860f24ec372b04e91d9bd1fe58837065b41
JaydenLet0720 <129974782+JaydenLet0720@users.noreply.github.com>
2024-02-20 11:51:13 -0500

    Update lsd.lua

--- lsd.lua
+++ lsd.lua
@@ -2,9 +2,22 @@ local lsd = {}
 lsd.utility = {}
 lsd.visuals = {}
 lsd.mouse = {}
-
+lsd.hooks = {}
 do
-  lsd.utility:LoadModule(path)
+  function lsd.utility:LoadModule(path)
     return require(path)
   end
+
+	function lsd.hooks:SetHook(hookname, func, hook)
+			lsd.hooks[hookname] = hook
+
+			hook = hookfunction(func, hook)
+		
+			return hook
+	 end
+
+	function lsd.hooks:RestoreHook(hookname)
+			local hook = table.find(lsd.hooks, hookname)
+			restorefunction(hook)
+	end
 end
